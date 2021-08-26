7f2ac5ccff3c2a2056eab6da4f7e9867d789f10b
Ciro Dourado <>
2021-08-26 11:46:42 -0300

    upando modelo para receber uma palavra aleatoria da API

---
+++ src/models/palavra_aleatoria.rs
@@ -0,0 +1,13 @@
+use serde::Deserialize;
+// puxando biblioteca de
+// manipulação de arquivos Json
+
+// aqui mandamos o compilador do Rust
+// implementar automaticamente a função
+// de deserializar para essa struct
+#[derive(Deserialize)]
+pub struct PalavraAleatoria {
+    sense: i32,
+    wid  : i32,
+    word : String
+} // struct PalavraAleatoria
