208af41875b630075a0342cb66efdf042b239f42
Ciro Dourado <>
2021-08-29 14:05:41 -0300

    separando funções em arquivos diferentes

--- src/cliente_https/cliente.rs
+++ src/cliente_https/cliente.rs
@@ -1,10 +1,9 @@
-use hyper::http::Result as HttpResult;
 use hyper::http::Error  as HttpError;
 use std::result::Result as StdResult;
-use hyper::Request;
-use hyper::Method;
-use hyper::Body;
 use hyper::Uri;
+
+use super::requisicao::montar_requisicao;
+use super::requisicao::fazer_requisicao;
 
 
 pub struct Cliente<'a> {
@@ -21,8 +20,8 @@ impl<'a> Cliente<'a> {
     pub fn get(&self, caminho: &'a str) -> String {
         self.montar_url(caminho)
             .and_then(montar_requisicao)
-            .map(fazer_requisicao)
-            .unwrap()
+            .map     (fazer_requisicao)
+            .unwrap_or(String::new())
     } // get
 
 
@@ -37,76 +36,3 @@ impl<'a> Cliente<'a> {
     } // montar_url
 
 } // impl Cliente
-
-
-fn montar_requisicao(url: Uri)
-    -> HttpResult<Request<Body>>
-{
-    Request::builder()
-        .method(Method::GET)
-        .uri(url)
-        .body(Body::from(r#"{"library":"hyper"}"#))
-} // montar_requisicao
-
-
-
-use hyper::client::HttpConnector;
-use hyper_tls::HttpsConnector;
-use hyper::Client;
-
-use futures::executor::block_on;
-
-
-fn fazer_requisicao(req: Request<Body>)
-    -> String
-{
-    let future = async move {
-        cliente_https().request(req).await
-    };
-    block_on(future)
-        .map(extrair_o_body)
-        .map(converter_em_bytes)
-        .map(converter_em_string)
-        .unwrap()
-} // fazer_requisicao
-
-
-fn cliente_https()
-    -> Client<HttpsConnector<HttpConnector>>
-{
-    let https   = HttpsConnector::new();
-    let cliente = Client::builder()
-        .build::<_, hyper::Body>(https);
-    return cliente;
-} // cliente_https
-
-
-
-use hyper::body::to_bytes;
-use hyper::body::Bytes;
-use hyper::Response;
-
-fn extrair_o_body(response: Response<Body>)
-    -> Body
-{
-    response.into_body()
-} // extrair_o_body
-
-
-fn converter_em_bytes(body: Body)
-    -> Bytes
-{
-    let future = async move { to_bytes(body).await };
-    match block_on(future) {
-        Ok(res) => res,
-        Err(_)  => Bytes::new() }
-} // converter_em_bytes
-
-
-fn converter_em_string(bytes: Bytes)
-    -> String
-{
-    let em_utf8 = bytes.to_vec();
-    String::from_utf8(em_utf8)
-        .unwrap_or(String::new())
-} // converter_em_string
