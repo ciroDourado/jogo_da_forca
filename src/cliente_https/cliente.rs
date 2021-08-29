9c850ea3003a9931891de71f5dc6ce4020866c28
Ciro Dourado <>
2021-08-29 10:54:17 -0300

    finalizando implementação do client

--- src/cliente_https/cliente.rs
+++ src/cliente_https/cliente.rs
@@ -21,8 +21,8 @@ impl<'a> Cliente<'a> {
     pub fn get(&self, caminho: &'a str) -> String {
         self.montar_url(caminho)
             .and_then(montar_requisicao)
-            .map(fazer_requisicao);
-        "a".to_string()
+            .map(fazer_requisicao)
+            .unwrap()
     } // get
 
 
@@ -49,23 +49,28 @@ fn montar_requisicao(url: Uri)
 } // montar_requisicao
 
 
+
+use hyper::client::HttpConnector;
+use hyper_tls::HttpsConnector;
+use hyper::Client;
+
 use futures::executor::block_on;
+
+
 fn fazer_requisicao(req: Request<Body>)
     -> String
 {
     let future = async move {
         cliente_https().request(req).await
     };
-    match block_on(future) {
-        Ok(res) => String::new() , // continuar daqui!!
-        Err(_)  => String::new() }
+    block_on(future)
+        .map(extrair_o_body)
+        .map(converter_em_bytes)
+        .map(converter_em_string)
+        .unwrap()
 } // fazer_requisicao
 
 
-
-use hyper::client::HttpConnector;
-use hyper_tls::HttpsConnector;
-use hyper::Client;
 fn cliente_https()
     -> Client<HttpsConnector<HttpConnector>>
 {
@@ -74,3 +79,34 @@ fn cliente_https()
         .build::<_, hyper::Body>(https);
     return cliente;
 } // cliente_https
+
+
+
+use hyper::body::to_bytes;
+use hyper::body::Bytes;
+use hyper::Response;
+
+fn extrair_o_body(response: Response<Body>)
+    -> Body
+{
+    response.into_body()
+} // extrair_o_body
+
+
+fn converter_em_bytes(body: Body)
+    -> Bytes
+{
+    let future = async move { to_bytes(body).await };
+    match block_on(future) {
+        Ok(res) => res,
+        Err(_)  => Bytes::new() }
+} // converter_em_bytes
+
+
+fn converter_em_string(bytes: Bytes)
+    -> String
+{
+    let em_utf8 = bytes.to_vec();
+    String::from_utf8(em_utf8)
+        .unwrap_or(String::new())
+} // converter_em_string
