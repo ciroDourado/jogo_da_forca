fd93d755009f6bad9a4d5a7a605d764c671cd1c4
Ciro Dourado <ciro.brz@gmail.com>
2021-08-29 18:49:04 -0300

    testando nova funcionalidade

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,36 @@
 pub mod cliente_https;
 pub mod models;
 
-use models::deserializacao::deserializar;
 use cliente_https::cliente::Cliente;
+
+use models::deserializacao::deserializar;
 use models::palavra_aleatoria::PalavraAleatoria;
+use models::significados::Significados;
 
 
 #[tokio::main]
 async fn main() -> () {
     let dominio = "api.dicionario-aberto.net";
     let cliente = Cliente::conectar_no(dominio);
-    let palavra = cliente.get("/random");
 
-    let aleatoria: PalavraAleatoria = deserializar(&palavra);
-    println!("{}", aleatoria.word);
+    let json_parte1 = cliente.get("/random");
+    let aleatoria   = deserializar::<PalavraAleatoria>(&json_parte1);
+    let buscada     = formatar( vec!["word", &aleatoria.word] );
+
+    let json_parte2  = cliente.get(&buscada);
+    let significados = deserializar::<Significados>(&json_parte2);
+
+    println!("{}", significados[0].word);
+    println!("{}", significados[0].xml);
 } // main
+
+
+fn formatar(partes: Vec<&str>) -> String {
+    let mut novas: Vec<String> = Vec::new();
+
+    for parte in partes {
+        let nova = urlencoding::encode(parte);
+        novas.push(nova.to_string());
+    }
+    format!("/{}/", novas.join("/"))
+}
