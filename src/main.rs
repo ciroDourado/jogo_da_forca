496339108b0f2430f212c1fcb971ea70c5c1b9e3
Ciro Dourado <>
2021-08-29 10:54:35 -0300

    testando o cliente https

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 pub mod cliente_https;
 use cliente_https::cliente::Cliente;
 
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() -> () {
+    let no_dominio = "api.dicionario-aberto.net";
+    let cliente    = Cliente::novo(no_dominio);
+
+    println!("{}", cliente.get("/random"));
+    println!("tudo certo até aqui :3");
 } // main
