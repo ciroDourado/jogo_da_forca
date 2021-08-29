b8675d9cfc7dda49b04902f003039c99cb0dd57f
Ciro Dourado <>
2021-08-29 15:00:05 -0300

    testando deserialização

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
+#![allow(dead_code)]
 pub mod cliente_https;
+pub mod models;
+
+use models::deserializacao::deserializar;
 use cliente_https::cliente::Cliente;
+use models::palavra_aleatoria::PalavraAleatoria;
 
 
 #[tokio::main]
 async fn main() -> () {
     let dominio = "api.dicionario-aberto.net";
     let cliente = Cliente::conectar_no(dominio);
+    let palavra = cliente.get("/random");
 
-    println!("{}", cliente.get("/random"));
-    println!("{}", cliente.get("/random"));
-    println!("{}", cliente.get("/é"));
-    println!("tudo certo até aqui :3");
+    let aleatoria: PalavraAleatoria = deserializar(&palavra);
+    println!("{}", aleatoria.word);
 } // main
